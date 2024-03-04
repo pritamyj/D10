002889236daec4da3f957fdfdeda5488edf5a276
pritamyj <jadhavpritam28@gmail.com>
2024-03-05 01:51:45 +0530

    Your Library playlist views updated

---
+++ web/modules/custom/your_library/src/Plugin/Block/YourLibrary.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\your_library\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\views\Views;
+
+/**
+ * Provides a 'Your' Block.
+ *
+ * @Block(
+ *   id = "your_library",
+ *   admin_label = @Translation("Your Library"),
+ *   category = @Translation("Your Library"),
+ * )
+ */
+
+class YourLibrary extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $content1['playlists']['view'] = views_embed_view('playlists', 'block_1');
+    $content2['following_artists']['view'] = views_embed_view('following_artists', 'block_1');
+    $result = array_merge($content1,$content2);
+
+      return [
+        '#theme' => 'hello_block',
+        '#data' => $result,
+        '#cache' => array(
+          'max-age' => 0,
+        ),
+      ];
+  }
+}
